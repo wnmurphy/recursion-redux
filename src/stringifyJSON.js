e27db8885879a3b20bba5cc8279424761aa1ba7d
Neil Murphy <wnmurphy@gmail.com>
2016-01-11 15:48:05 -0800

    Add stringifyJSON.js

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,36 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+
+  // handle strings w ""
+  if(typeof obj === 'string'){
+    return '"' + obj + '"';
+  }
+  
+  // handle arrays w [] / recursive
+  if(Array.isArray(obj)){
+  	var results = [];
+  	for (var i = 0; i < obj.length; i += 1) {
+  		results.push(stringifyJSON(obj[i]));
+  	}
+    return '[' + results.join(',') + ']';
+  }
+  
+  // handle objects w {} / recursive
+  if(typeof obj === "object" && obj !== null){
+  	var results = [];
+  	if(Object.keys(obj).length === 0){
+  	  return '{}';
+  	}
+  	for (var prop in obj){
+  		if(typeof obj[prop] === 'undefined' || typeof obj[prop] === 'function'){
+  			continue;
+  		} else {
+  			results.push(stringifyJSON(prop) + ':' + stringifyJSON(obj[prop]));
+  		}
+  	}
+    return '{' + results.join(',') + '}';
+  }
+  
+  return '' + obj;
 };
